aa030f4de77aa1c95e4f2edb67feaea01f4f3cca
Valderruten <valderruten@gmail.comgit config --global user.email valderruten@gmail.comgit config --global user.email valderruten@gmail.com>
2025-09-10 16:07:30 -0500

    Ajustar formato de entrada en la interfaz de usuario y corregir valor predeterminado para 'Alcohol'

--- app.py
+++ app.py
@@ -32,9 +32,8 @@ tab1, tab2 = st.tabs(["Características Físico-químicas", "Componentes Fenóli
 user_inputs_dict = {}
 
 # Definir los nombres y valores predeterminados para los campos de entrada
-# Esto hace que la interfaz se vea más profesional y sea más fácil de usar
 input_params = {
-    'Alcohol': 13.0,
+    'Alcohol': 13.09,
     'Malic acid': 2.0,
     'Ash': 2.0,
     'Alcalinity of ash': 17.0,
@@ -53,26 +52,26 @@ with tab1:
     st.header("Características Físico-químicas")
     col1, col2 = st.columns(2)
     with col1:
-        user_inputs_dict['Alcohol'] = st.number_input('Alcohol', value=input_params['Alcohol'], step=0.01, format="%.2f")
-        user_inputs_dict['Malic acid'] = st.number_input('Malic acid', value=input_params['Malic acid'], step=0.01, format="%.2f")
-        user_inputs_dict['Ash'] = st.number_input('Ash', value=input_params['Ash'], step=0.01, format="%.2f")
-        user_inputs_dict['Alcalinity of ash'] = st.number_input('Alcalinity of ash', value=input_params['Alcalinity of ash'], step=0.01, format="%.2f")
+        user_inputs_dict['Alcohol'] = st.number_input('Alcohol', value=input_params['Alcohol'], step=0.01)
+        user_inputs_dict['Malic acid'] = st.number_input('Malic acid', value=input_params['Malic acid'], step=0.01)
+        user_inputs_dict['Ash'] = st.number_input('Ash', value=input_params['Ash'], step=0.01)
+        user_inputs_dict['Alcalinity of ash'] = st.number_input('Alcalinity of ash', value=input_params['Alcalinity of ash'], step=0.01)
     with col2:
-        user_inputs_dict['Magnesium'] = st.number_input('Magnesium', value=input_params['Magnesium'], step=1.0, format="%d")
-        user_inputs_dict['Color intensity'] = st.number_input('Color intensity', value=input_params['Color intensity'], step=0.01, format="%.2f")
-        user_inputs_dict['Hue'] = st.number_input('Hue', value=input_params['Hue'], step=0.01, format="%.2f")
+        user_inputs_dict['Magnesium'] = st.number_input('Magnesium', value=input_params['Magnesium'], step=1.0)
+        user_inputs_dict['Color intensity'] = st.number_input('Color intensity', value=input_params['Color intensity'], step=0.01)
+        user_inputs_dict['Hue'] = st.number_input('Hue', value=input_params['Hue'], step=0.01)
 
 with tab2:
     st.header("Componentes Fenólicos y Prolinas")
     col1, col2 = st.columns(2)
     with col1:
-        user_inputs_dict['Total phenols'] = st.number_input('Total phenols', value=input_params['Total phenols'], step=0.01, format="%.2f")
-        user_inputs_dict['Flavanoids'] = st.number_input('Flavanoids', value=input_params['Flavanoids'], step=0.01, format="%.2f")
-        user_inputs_dict['Nonflavanoid phenols'] = st.number_input('Nonflavanoid phenols', value=input_params['Nonflavanoid phenols'], step=0.01, format="%.2f")
+        user_inputs_dict['Total phenols'] = st.number_input('Total phenols', value=input_params['Total phenols'], step=0.01)
+        user_inputs_dict['Flavanoids'] = st.number_input('Flavanoids', value=input_params['Flavanoids'], step=0.01)
+        user_inputs_dict['Nonflavanoid phenols'] = st.number_input('Nonflavanoid phenols', value=input_params['Nonflavanoid phenols'], step=0.01)
     with col2:
-        user_inputs_dict['Proanthocyanins'] = st.number_input('Proanthocyanins', value=input_params['Proanthocyanins'], step=0.01, format="%.2f")
-        user_inputs_dict['OD280/OD315 of diluted wines'] = st.number_input('OD280/OD315 of diluted wines', value=input_params['OD280/OD315 of diluted wines'], step=0.01, format="%.2f")
-        user_inputs_dict['Proline'] = st.number_input('Proline', value=input_params['Proline'], step=1.0, format="%d")
+        user_inputs_dict['Proanthocyanins'] = st.number_input('Proanthocyanins', value=input_params['Proanthocyanins'], step=0.01)
+        user_inputs_dict['OD280/OD315 of diluted wines'] = st.number_input('OD280/OD315 of diluted wines', value=input_params['OD280/OD315 of diluted wines'], step=0.01)
+        user_inputs_dict['Proline'] = st.number_input('Proline', value=input_params['Proline'], step=1.0)
 
 # Botón y resultado
 if st.button('Predecir Variedad'):
